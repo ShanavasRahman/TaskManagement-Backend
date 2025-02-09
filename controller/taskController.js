7bdfb0bbc4617e67c0011cebab30001d89078ea1
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2025-02-09 21:05:02 +0530

    task crud almost completed

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,70 +2,87 @@ const User = require("../model/userModel");
 const Task = require("../model/taskModel");
 
 const createTask = async (req, res) => {
-    try {
-        const { title, description, dueDate, priority, userId } = req.body;
-        
-        const user = await User.findById(userId);
-        
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-    
-        const task = new Task({
-            title,
-            description,
-            dueDate,
-            priority,
-            user: userId 
-        });
-    
-        await task.save();
-    
-        user.tasks.push(task._id);
-        await user.save();  
-    
-        return res.status(201).json({ message: "Task created successfully" });
-    
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Internal server error" });
+  try {
+    const { title, description, dueDate, priority, userId } = req.body;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    
-}
+
+    const task = new Task({
+      title,
+      description,
+      dueDate,
+      priority,
+      user: userId,
+    });
+
+    await task.save();
+
+    user.tasks.push(task._id);
+    await user.save();
+
+    return res.status(201).json({ message: "Task created successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 const displayTask = async (req, res) => {
-    try {
-        const userId = req.params.id;
+  try {
+    const userId = req.params.id;
 
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const tasks = await Task.find({ user: userId });
-
-        return res.status(200).json({
-            message: "Tasks successfully fetched",
-            tasks: tasks  
-        });
-
-    } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    const tasks = await Task.find({ user: userId });
+
+    return res.status(200).json({
+      message: "Tasks successfully fetched",
+      tasks: tasks,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 const updateTask = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
- 
-    }
+  try {
+    const taskId = req.params.id;
+    const { title, description, priority } = req.body;
+    const updatedTask = await Task.findByIdAndUpdate(
+      taskId,
+      { $set: { title, description, priority } },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ message: "task updated successfully", task: updatedTask });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-
+const deleteTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const deletedTask = await Task.findByIdAndDelete(taskId);
+    return res
+      .status(200)
+      .json({ message: "task deleted successfully", task: deletedTask });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
-    createTask,
-    displayTask
-}
+  createTask,
+  displayTask,
+  updateTask,
+  deleteTask,
+};
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { userSignUp } = require("../controller/userController");
-const { createTask, displayTask } = require("../controller/taskController");
+const { createTask, displayTask, updateTask, deleteTask } = require("../controller/taskController");
 const route = express.Router();
 
 route.post("/signup", userSignUp);
 route.post("/addtask", createTask);
 route.get("/displaytask/:id", displayTask);
+route.put("/updatetask/:id", updateTask);
+route.post("/deletetask/:id", deleteTask);
 
 module.exports = route;
